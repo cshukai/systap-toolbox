b95a24ec1e4cb654a0b469846d757c20cf009e8f
OpenShift guest <sc724@mail.missouri.edu>
2014-03-20 15:59:31 -0400

    fix module import : adding keyword back

--- web.js
+++ web.js
@@ -1,3 +1,8 @@
 var importer=require("./lib/import");
+var reformatter=require("./lib/reformat");
 
-importer.importNtriple("test");
+//data preprocessing for omim file
+var patt=/\\'/g;
+var replacement=' ';
+reformatter.replaceString('omim.nt','omim.parsed.nt',patt,replacement);
+
